ea0bb340d7539050eaf30da49177868693743a8f
Daniel Roman <roman.martinez.daniel@gmail.com>
2017-01-16 22:50:37 +0100

    Lesson 3

---
+++ Lesson3/ejemplo.php
@@ -0,0 +1,51 @@
+<?php
+
+// Unit
+abstract class Unit {
+	protected $alive = true;
+	protected $name;
+
+	public function __construct($name){
+		$this->name = $name;
+	}
+
+	public function move($direction){
+		echo "<p>{$this->name} camina hacia $direction</p>";
+	}
+
+	abstract public function attack($opponent);
+}
+
+// Soldier
+class Soldier extends Unit{
+
+	public function attack($opponent){
+		echo "<p>{$this->name} corta a $opponent en 2</p>";	
+	}
+}
+
+// Archer
+class Archer extends Unit{
+	public function attack($opponent){
+		echo "<p>{$this->name} dispara una flecha a $opponent</p>";	
+	}
+}
+
+/*
+$dk = new Unit('Dani');
+$dk->move('Hacia el sud');
+$dk->attack('Ramm');
+*/
+echo "<hr>";
+
+$dk = new Soldier('Dani');
+$dk->move('Hacia el sud');
+$dk->attack('Ramm');
+
+echo "<hr>";
+
+$dk = new Archer('Dani');
+$dk->move('Hacia el sud');
+$dk->attack('Ramm');
+
+?>
